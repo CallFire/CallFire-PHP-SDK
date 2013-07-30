7230a50a52f4b1d4f364b1e02139385c4748032c
Justin Martin <justin@roave.com>
2013-07-30 11:21:00 -0700

    Added supporting files.

---
+++ tools/rest-generator.php
@@ -0,0 +1,51 @@
+<?php
+namespace CallFire\Generator;
+
+use CallFire\Generator\Soap as SoapGenerator;
+use CallFire\Generator\Rest as RestGenerator;
+use Zend\Code\Generator\ClassGenerator;
+use Zend\Code\Generator\FileGenerator;
+use Zend\Code\Generator\ValueGenerator;
+
+require __DIR__."/../vendor/autoload.php";
+
+$name = "Client";
+$namespace = "CallFire\Api";
+$restNamespace = "Rest";
+$extendedClass = "";
+$swaggerUrl = 'https://www.callfire.com/api/1.1/wsdl/swagger';
+
+$requestNamespacePart = SoapGenerator::REQUEST_NAMESPACE_ALIAS;
+$requestNamespace = "{$namespace}\\{$requestNamespacePart}";
+
+$responseNamespacePart = SoapGenerator::RESPONSE_NAMESPACE_ALIAS;
+$responseNamespace = "{$namespace}\\{$responseNamespacePart}";
+
+$structureNamespacePart = SoapGenerator::STRUCTURE_NAMESPACE_ALIAS;
+$structureNamespace = "{$namespace}\\{$structureNamespacePart}";
+
+$sourceDirectory = realpath(__DIR__."/../src").'/'.str_replace('\\', '/', $namespace);
+
+$generator = new RestGenerator($swaggerUrl);
+
+$classGenerator = new ClassGenerator($name, "{$namespace}\\{$restNamespace}", null, $extendedClass);
+$classGenerator->addUse($extendedClass);
+$generator->setClassGenerator($classGenerator);
+
+$generator->generateFunctions($requestNamespace);
+$generator->generateClasses($requestNamespace, $responseNamespace, $structureNamespace);
+$classFiles = $generator->generateClassFiles();
+
+if(!is_dir($sourceDirectory)) {
+    mkdir($sourceDirectory, 0777, true);
+}
+if(!is_dir("{$sourceDirectory}/{$restNamespace}")) {
+    mkdir("{$sourceDirectory}/{$restNamespace}", 0777, true);
+}
+
+foreach($classFiles as $classFile) {
+    $classFile->setFilename("{$sourceDirectory}/{$restNamespace}/{$classFile->getClass()->getName()}.php");
+    $classFile->write();
+}
+
+passthru('php '.__DIR__.'/../vendor/fabpot/php-cs-fixer/php-cs-fixer fix '.__DIR__.'/../src/CallFire/Api/Rest/ --level=all');

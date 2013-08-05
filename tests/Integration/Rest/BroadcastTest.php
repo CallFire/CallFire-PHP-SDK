9354b096f5f871c22363d2478f37ec9f820c5099
Justin Martin <justin@roave.com>
2013-08-05 16:09:23 -0700

    Added Broadcast resource assertions.

--- tests/Integration/Rest/BroadcastTest.php
+++ tests/Integration/Rest/BroadcastTest.php
@@ -17,6 +17,25 @@ class BroadcastTest extends TestCase
         $this->assertStringStartsWith("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<r:ResourceList", $response);
         
         $broadcasts = $client::response($response);
+        foreach($broadcasts as $broadcast) {
+            $this->assertInstanceOf('CallFire\\Common\\Resource\\Broadcast', $broadcast);
+            $this->assertNotEmpty($broadcast->getId());
+            $this->assertNotEmpty($broadcast->getName());
+            $this->assertNotEmpty($broadcast->getStatus());
+            $this->assertNotEmpty($broadcast->getType());
+            
+            if($ivrBroadcastConfig = $broadcast->getIvrBroadcastConfig()) {
+                $this->assertInstanceOf('CallFire\\Common\\Resource\\IvrBroadcastConfig', $ivrBroadcastConfig);
+                $this->assertNotEmpty($ivrBroadcastConfig->getFromNumber());
+                $this->assertNotEmpty($ivrBroadcastConfig->getDialplanXml());
+                
+                if($retryConfig = $ivrBroadcastConfig->getRetryConfig()) {
+                    $this->assertInstanceOf('CallFire\\Common\\Resource\\RetryConfig', $retryConfig);
+                    $this->assertNotEmpty($retryConfig->getMaxAttempts());
+                    $this->assertNotEmpty($retryConfig->getMinutesBetweenAttempts());
+                }
+            }
+        }
     }
 
     public function getBroadcastClient()

0d4d264b9b876bbd9fc8a3a09e9836af0aa4e3d7
Justin Martin <justin@roave.com>
2013-08-06 12:40:41 -0700

    Moved resource parsing to abstract response. Added individual Resource response parsing.

---
+++ src/CallFire/Api/Rest/Response/Resource.php
@@ -0,0 +1,57 @@
+<?php
+namespace CallFire\Api\Rest\Response;
+
+use CallFire\Api\Rest\Response as AbstractResponse;
+use CallFire\Common\AbstractResource;
+
+use CallFire\Common\Hydrator\DOM as DomHydrator;
+use Zend\Stdlib\Hydrator\ClassMethods;
+
+use DOMDocument;
+use DOMNode;
+use LogicException;
+
+class Resource extends AbstractResponse
+{
+    protected $resource;
+    
+    public static function fromXml(DOMDocument $document)
+    {
+        $resourceList = new self;
+        $resourceList->loadXml($document);
+
+        return $resourceList;
+    }
+
+    public function loadXml(DOMDocument $document, DOMNode $contextNode = null, $cleanup = true)
+    {
+        if (!($xpath = $this->getXPath())) {
+            $xpath = static::createXPath($document);
+            $this->setXPath($xpath);
+        }
+
+        if (!$contextNode) {
+            $contextNode = $xpath->query('/r:Resource')->item(0);
+        }
+
+        $resourceNode = $xpath->query('*[1]', $contextNode);
+        $resource = $this->parseResourceNode($resourceNode);
+        if ($resource instanceof AbstractResource) {
+            $this->setResource($resource);
+        }
+
+        if ($cleanup) {
+            $this->setXPath(null);
+            $this->setDomHydrator(null);
+        }
+    }
+    
+    public function getResource() {
+        return $this->resource;
+    }
+    
+    public function setResource(AbstractResource $resource) {
+        $this->resource = $resource;
+        return $this;
+    }
+}
--- src/CallFire/Api/Rest/Response/ResourceList.php
+++ src/CallFire/Api/Rest/Response/ResourceList.php
@@ -19,14 +19,6 @@ class ResourceList extends AbstractResponse implements IteratorAggregate
     
     protected $resources = array();
 
-    protected $xpath;
-
-    protected $hydrator;
-
-    protected $domHydrator;
-
-    protected $queryMap;
-
     public static function fromXml(DOMDocument $document)
     {
         $resourceList = new self;
@@ -64,58 +56,6 @@ class ResourceList extends AbstractResponse implements IteratorAggregate
         }
     }
 
-    public function parseResourceNode(DOMNode $resourceNode)
-    {
-        $xpath = $this->getXPath();
-        $hydrator = $this->getDomHydrator();
-        $methodsHydrator = $this->getHydrator();
-
-        $queryMap = $this->getQueryMap();
-        if (!$queryMap) {
-            throw new LogicException('Unable to access query map');
-        }
-
-        $resourceName = $resourceNode->nodeName;
-        if (!isset($queryMap[$resourceName])) {
-            return false;
-        }
-        $resourceMap = $queryMap[$resourceName];
-        $hydrator->setQueryMap($resourceMap);
-
-        $childResourceMap = array();
-        foreach ($resourceMap as $key => $query) {
-            if (substr($key, 0, 1) == '#') {
-                unset($resourceMap[$key]);
-                $childResourceMap[substr($key, 1)] = $query;
-            }
-        }
-
-        $resourceClassName = "CallFire\\Common\\Resource\\{$resourceName}";
-        if (!class_exists($resourceClassName)) {
-            return false;
-        }
-
-        $resourceData = $hydrator->extract($resourceNode);
-        $resource = $methodsHydrator->hydrate($resourceData, new $resourceClassName);
-        foreach ($childResourceMap as $key => $query) {
-            $childResourceNode = $xpath->query($query, $resourceNode)->item(0);
-            if (!$childResourceNode) {
-                continue;
-            }
-
-            $childResource = $this->parseResourceNode($childResourceNode);
-            if (!$childResource) {
-                continue;
-            }
-
-            $methodsHydrator->hydrate(array(
-                $key => $childResource
-            ), $resource);
-        }
-
-        return $resource;
-    }
-
     public function getIterator()
     {
         return new ArrayIterator($this->getResources());
@@ -147,78 +87,5 @@ class ResourceList extends AbstractResponse implements IteratorAggregate
         $this->resources[] = $resource;
 
         return $this;
-    }
-
-    public function getXPath()
-    {
-        return $this->xpath;
-    }
-
-    public function setXPath($xpath)
-    {
-        $this->xpath = $xpath;
-
-        return $this;
-    }
-
-    public function getHydrator()
-    {
-        if (!$this->hydrator) {
-            $this->hydrator = new ClassMethods;
-        }
-
-        return $this->hydrator;
-    }
-
-    public function setHydrator($hydrator)
-    {
-        $this->hydrator = $hydrator;
-
-        return $this;
-    }
-
-    public function getDomHydrator()
-    {
-        if (!$this->domHydrator) {
-            $this->domHydrator = new DomHydrator;
-            if ($xpath = $this->getXPath()) {
-                $this->domHydrator->setXPath($xpath);
-            }
-        }
-
-        return $this->domHydrator;
-    }
-
-    public function setDomHydrator($domHydrator)
-    {
-        $this->domHydrator = $domHydrator;
-
-        return $this;
-    }
-
-    public function getQueryMap()
-    {
-        if (!$this->queryMap) {
-            $this->queryMap = static::loadQueryMap();
-        }
-
-        return $this->queryMap;
-    }
-
-    public function setQueryMap($queryMap)
-    {
-        $this->queryMap = $queryMap;
-
-        return $this;
-    }
-
-    private static function loadQueryMap()
-    {
-        $queryMap = include dirname(__DIR__).'/querymap.php';
-        if ($queryMap) {
-            return $queryMap;
-        }
-
-        return null;
     }
 }

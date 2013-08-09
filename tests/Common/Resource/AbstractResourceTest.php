9a87bfd1d8b97f262497ed48f0e4291cfedd7656
Justin Martin <justin@roave.com>
2013-08-09 09:50:29 -0700

    Resource instantiation test.

---
+++ tests/Common/Resource/AbstractResourceTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace CallFire\Common\Resource;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+class AbstractResourceTest extends TestCase
+{
+    public function testResourceInstantiation()
+    {
+        foreach(glob(__DIR__.'/../../../src/Common/Resource/*.php') as $filename)
+        {
+            $className = basename($filename);
+            if($className == 'AbstractResource') {
+                continue;
+            }
+            
+            $qualifiedClassName = "CallFire\\Common\\Resource\\{$className}";
+            
+            $resource = new $qualifiedClassName;
+            $this->assertInstanceOf($qualifiedClassName, $resource);
+        }
+    }
+}

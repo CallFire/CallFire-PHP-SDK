c5dfcc25e4076db41817e50a94f726cae5eca0f8
Justin Martin <jmartin@callfire.com>
2013-08-23 16:49:56 -0700

    Test that the tag type in PHP is retained for the age of the DOMDocument.

--- tests/Ivr/IvrTest.php
+++ tests/Ivr/IvrTest.php
@@ -5,6 +5,7 @@ use PHPUnit_Framework_TestCase as TestCase;
 
 use DOMDocument;
 use DOMNode;
+use DOMXPath;
 
 class IvrTest extends TestCase
 {
@@ -14,5 +15,27 @@ class IvrTest extends TestCase
         $dialplan = $ivr->getDialplan();
         
         $this->assertInstanceOf('CallFire\Common\Ivr\Dialplan', $dialplan);
+    }
+    
+    public function testTagTypeNotLost()
+    {
+        $ivr = new Ivr;
+        $dialplan = $ivr->getDialplan();
+        $tag = $dialplan->Amd();
+        
+        $blindTag = $dialplan->firstChild;
+        $this->assertInstanceOf(get_class($tag), $blindTag);
+    }
+    
+    public function testTagTypeNotLostOnQuery()
+    {
+        $ivr = new Ivr;
+        $dialplan = $ivr->getDialplan();
+        $tag = $dialplan->Amd();
+        
+        $xpath = new DOMXPath($ivr);
+        $blindTag = $xpath->query($tag::NODE_NAME, $dialplan)->item(0);
+        
+        $this->assertInstanceOf(get_clasS($tag), $blindTag);
     }
 }

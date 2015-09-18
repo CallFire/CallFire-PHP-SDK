1d5ff3e6ea72236bb8dc0fd69eaa41c938318bc6
Justin Martin <jmartin@callfire.com>
2015-09-18 10:03:30 -0700

    Very early functioning client.

---
+++ src/CallFire/Api/Client.php
@@ -0,0 +1,26 @@
+<?php
+namespace CallFire\Api;
+
+use Swagger\Client as SwaggerClient;
+
+class Client extends SwaggerClient
+{
+    public static $callFireSwaggerUrl = 'https://www.callfire.com/api/v2/api-docs/swagger.json';
+
+    public static function createClient(
+        $appLogin,
+        $appPassword
+    )
+    {
+        $client = static::create(static::$callFireSwaggerUrl);
+        
+        $client->setDefaultSecurityCredential(
+            $client->createBasicCredential(
+                $appLogin,
+                $appPassword
+            )
+        );
+        
+        return $client;
+    }
+}
